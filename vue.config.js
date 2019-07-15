a3ae6180a687c582a2fc851de7731085e8559755
baseon2016 <baseon_000@163.com>
2019-07-15 23:12:32 +0800

    更新vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  // 当vue的环境处于 production (生产环境)时，将 publicPath 改成 /Vue-js-shequ , 开发环境是 /
+  // 当vue的环境处于 production (生产环境)时，将 publicPath 改成 /vue-shequ , 开发环境是 /
   publicPath: process.env.NODE_ENV === "production" ? "/vue-shequ/" : "/"
 };
